63365bcd135abbbe12a8fb1665f845db94912e3f
mpelaezr <mmpelaezr@gmail.com>
2021-09-20 20:47:54 -0500

    version 1

---
+++ js/jquery.js
@@ -0,0 +1,92 @@
+$(".empresa1").css("color", "green")
+
+$(".empresa1").mouseover(function(){
+
+    $(".empresa1").css ("background", "pink").css("color","black")
+    })
+
+    $(".empresa1").mouseout(function(){
+
+        $(".empresa1").css ("background", "red").css("color","black")
+        })
+
+
+
+$(".empresa2").click( function(){
+        $(".empresa2").css("font-size","30px") }
+)
+$(".empresa2").mouseover(function(){
+
+    $(".empresa2").css ("background", "green").css("color","black")
+    })
+
+
+
+
+
+$(".empresa3").dblclick (
+    function(){
+        $(".empresa3").css("blackground", "red").css ("color", "red")  }
+)
+
+$(".empresa3").dblclick (
+    function(){
+        $(".empresa3").css("blackground", "red").css ("color", "red")  }
+)
+
+
+
+
+$(".empresa4").mouseover( function(){
+        $(".empresa4").css("backgrounp","pink").css("color","green") }
+)
+$(".empresa4").mouseout(function (){
+        $(".empresa4").css("backgrounp", "white").css("color", "purple") }
+)
+
+
+$(".empresa5").css("color", "orange")
+
+$(".empresa6").ready ( function(){
+    $("empresa6").css ("font-size", "40px");}
+) 
+
+$(".empresa7").css("color", "green")
+
+$(".empresa8").click(function(){
+        $(".empresa2").css("font-size","30px").css ("color", "orange") }
+)
+$(".empresa8").click( function(){
+    $(".empresa8").css("font-size","30px") }
+)
+
+$(".empresa9").css("color", "purple")
+
+
+
+$(".empresa10").css("color", "gray")
+
+$(".empresa10").mouseover(function(){
+
+    $(".empresa10").css ("background", "purple").css("color","black")
+    })
+
+    $(".empresa10").mouseout(function(){
+
+        $(".empresa10").css ("background", "futsia").css("color","black")
+        })
+
+
+
+
+$(document).ready(function(){
+
+    $(".parrafo2") .css ("background", "pink");
+    })
+
+$(".parrafo2").click(
+        function(){
+            $(".parrafo2").css("font-size","30px") }
+    )
+
+
